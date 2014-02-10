56bc49abe2b221c98f853609e239406a5d978783
Melinda Mizuta <mkmizuta@gmail.com>
2014-02-10 13:35:30 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PeaPatchPlanner::Application.config.secret_key_base = '053659ee54e803394d7a1f68018398c6d5906f4302148546fa6b2ae1a6fa5b341c851e2d4dc7e4da6c44588ff7429b13e4e8734159ceb234bb06be2418835076'
