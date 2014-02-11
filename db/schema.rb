69e23e14d1e64eb6701db334f54f25f814fc79d5
Shadae Holmes <ShadaeHolmes@gmail.com>
2014-02-11 11:35:50 -0800

    Session Controller added and working' '

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140210223344) do
+ActiveRecord::Schema.define(version: 20140211180655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140210223344) do
     t.string  "uid"
     t.string  "email"
     t.boolean "admin"
+    t.string  "password_digest"
   end
 
 end
