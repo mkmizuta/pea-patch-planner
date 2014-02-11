11464767abf96cad94256f7b02d53d92d8e88adb
Melinda Mizuta <mkmizuta@gmail.com>
2014-02-10 16:15:43 -0800

    completed user model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\w+@+\w+\.+\w+/ }
   validates :uid, presence: true, uniqueness: true, format: { with: /\w+/}
 end
