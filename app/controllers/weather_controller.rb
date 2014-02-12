b28185fab5a14951e8ed37b868ebe20a662d47ef
Whitney Levis <ms.whitney.levis@gmail.com>
2014-02-12 09:02:14 -0800

    Evening adventures in setting up weather creation methods

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,7 +1,26 @@
 class WeatherController < ApplicationController
 
   def create
-    
+    create_forecast
+    (0..2).each do |place_value|
+      create_day(place_value)
+      @day.save
+    end
+    redirect_to root_path
   end
 
+  private
+
+  def create_forecast
+    @forecast = Weather.new
+    @forecast.dt = Time.now
+    @forecast.save
+  end
+
+  def create_day(place_value)
+    @day = DaysWeather.new
+    @day.forcast_association = Weather.last.id
+    @day.temp = DaysWeather.forecast_temp
+    @day.description = DaysWeather.forecast_description
+  end
 end
--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,4 +1,6 @@
 class Weather < ActiveRecord::Base
+  has_many :days_weathers
+  validates :dt, presence: true
 
   # Methods that allow access to current weather data
 
@@ -14,27 +16,5 @@ class Weather < ActiveRecord::Base
     current_hash["main"]["temp"]
   end
 
-  # Methods that allow access to forecast weather data
 
-  def forecast_array
-    HTTParty.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=Seattle&cnt=3&units=imperial&mode=json").parsed_response["list"]
-  end
-
-  def forecast_description
-    description_array = []
-    forecast_array.each do |day|
-      description = day["weather"][0]["main"]
-      description_array << description
-    end
-    description_array
-  end
-
-  def forecast_temp
-    temp_array = []
-    forecast_array.each do |day|
-      temp = day["temp"]["day"]
-      temp_array << temp
-    end
-    temp_array
-  end
 end
