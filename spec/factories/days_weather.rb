ac21d600c58e132e6870f07f656543159dab0dad
Whitney Levis <ms.whitney.levis@gmail.com>
2014-02-13 12:19:02 -0800

    Weather Controller specs written

--- spec/factories/days_weather.rb
+++ spec/factories/days_weather.rb
@@ -8,8 +8,3 @@ FactoryGirl.define do
   end
 
 end
-  # belongs_to :weather
-  # validates :temp, presence: true
-  # validates :description, presence: true
-  # validates :place_value, presence: true
-  # validates :forecast_association, presence:true
