47b21a2a097a3363bcc8e8d6bd927ef1346400fd
Whitney Levis <ms.whitney.levis@gmail.com>
2014-02-13 09:11:27 -0800

    days_weather model validations

--- spec/factories/days_weather.rb
+++ spec/factories/days_weather.rb
@@ -2,12 +2,12 @@ FactoryGirl.define do
   factory :days_weather do
     temp                '32'
     description         'Snow'
-    place_value
-    forecast_association
+    place_value          1
+    forecast_association 1
 
   end
 
-
+end
   # belongs_to :weather
   # validates :temp, presence: true
   # validates :description, presence: true
--- spec/models/days_weather_spec.rb
+++ spec/models/days_weather_spec.rb
@@ -3,12 +3,36 @@ require 'spec_helper'
 describe DaysWeather do
   let(:days_weather) {create(:days_weather)}
 
+  describe "validations" do
+    
+    it "is valid" do
+      expect(days_weather).to be_valid
+    end
 
+    it "has a temp" do
+      days_weather.update(temp: nil)
+      expect(days_weather.errors[:temp]).to_not be_empty
+    end
+
+    it "has a description" do
+      days_weather.update(description: nil)
+      expect(days_weather.errors[:description]).to_not be_empty
+    end
+
+    it "has a place_value" do
+      days_weather.update(place_value: nil)
+      expect(days_weather.errors[:place_value]).to_not be_empty
+    end
+
+    it "has a forecast_association" do
+      days_weather.update(forecast_association: nil)
+      expect(days_weather.errors[:forecast_association]).to_not be_empty
+    end
+  end
+
+  describe "association" do
+    it "responds to #weather (forecast)" do
+      expect(days_weather).to respond_to(:weather)
+    end
+  end
 end
-
-
-  # belongs_to :weather
-  # validates :temp, presence: true
-  # validates :description, presence: true
-  # validates :place_value, presence: true
-  # validates :forecast_association, presence:true
