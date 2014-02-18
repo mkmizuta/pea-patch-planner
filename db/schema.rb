4dd318811326bae0392fab1b26face039ce38e68
Whitney Levis <ms.whitney.levis@gmail.com>
2014-02-18 09:38:25 -0800

    Mid-process commit to merge from master

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140213182444) do
+ActiveRecord::Schema.define(version: 20140218165756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 20140213182444) do
     t.float   "temp"
     t.string  "icon"
     t.integer "place_value"
+  end
+
+  create_table "tools", force: true do |t|
+    t.string  "name"
+    t.text    "description"
+    t.integer "owner"
   end
 
   create_table "users", force: true do |t|
---
+++ spec/factories/tool.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :tool do
+    name "shovel"
+    description "5 feet long, good for digging"
+  end  
+end
