a46b7ba27bb46deab99106bf4298aba02532d1ea
Shadae Holmes <ShadaeHolmes@gmail.com>
2014-02-19 09:02:41 -0800

    Mailer Update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,14 @@
 PeaPatchPlanner::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              "smtp.mandrillapp.com",
+  port:                 587,
+  enable_starttls_auto: true,
+  user_name:            ENV["MANDRILL_USERNAME"], # YOUR MANDRILL USERNAME
+  password:             ENV["MANDRILL_PASSWORD"], # A MANDRILL API KEY
+  authentication:       'login',
+  domain:               'fauxSeattleStoneSoup.com'
+}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -14,7 +24,8 @@ PeaPatchPlanner::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
