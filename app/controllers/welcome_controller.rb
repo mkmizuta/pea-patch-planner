65563656e701575db80905a8d185c8afe748e1c0
Whitney Levis <ms.whitney.levis@gmail.com>
2014-02-19 20:00:27 -0800

    Weather works without having to manually update... also without resque? :)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,49 @@
 class WelcomeController < ApplicationController
   def index
     @weather = Weather.new
+    check_and_update_weather
+  end
+
+  def check_and_update_weather
+    if Weather.last
+      old = Weather.last.dt
+      current = Time.now
+      if ((current).minus_with_coercion(old)/3600).round >= 24
+        update_weather
+      end
+    else
+      update_weather
+    end
+
+
+  end
+
+  def update_weather
+    create_forecast
+    (0..2).each do |place_value|
+      create_day(place_value)
+    end
+    redirect_to root_path
+  end
+
+  def create_forecast
+    @forecast = Weather.new
+    @forecast.dt = Time.now
+    if @forecast.save
+    else
+      flash[:notice] = "Forecast could not be saved"
+    end
+  end
+
+  def create_day(place_value)
+    @day = DaysWeather.new
+    @day.forecast_association = Weather.last.id
+    @day.temp = @day.forecast_temp(place_value)
+    @day.description = @day.forecast_description(place_value)
+    @day.place_value = place_value
+    if @day.save
+    else
+      flash[:notice] = "This day's could not be saved"
+    end
   end
 end
