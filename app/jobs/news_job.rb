2cf2a7569a344c2d211b9f43c8b95115f8fd25b3
Shadae Holmes <ShadaeHolmes@gmail.com>
2014-02-21 18:23:14 -0800

    Mailer working locally, and html fixes

---
+++ app/jobs/news_job.rb
@@ -0,0 +1,6 @@
+class NewsJob
+  @queue = :email
+  def self.perform(post_id, user_id)
+    UserMailer.newpost(post_id, user_id).deliver
+  end
+end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,14 @@
 class UserMailer < ActionMailer::Base
-    default from: "Info@SeattleStoneSoup.com"
+  default from: "Info@SeattleStoneSoup.com"
 
   def welcome(user_id)
     @user = User.find(user_id)
     mail(to: @user.email, subject: "Hi #{@user.username}! Thank you for joining Seattle Stone Soup the Community Garden")
+  end
+
+  def newpost(blogpost_id, user_id)
+    @blogpost_id = blogpost_id
+    @user = User.find(user_id)
+    mail(to: "#{@user.email}", subject: "Hey #{@user.username}! New Garden News posted! Check it out!")
   end
 end
